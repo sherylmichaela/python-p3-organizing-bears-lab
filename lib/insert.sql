81000ba77e2dace6ade26e17ebbac141e2d332be
Sheryl Chee <sherylmichaela@gmail.com>
2024-05-18 01:33:44 +1000

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 2, "M", "dark brown", "Good", 1),
+(2, "Rowdy", 1, "M", "black", "Happy", 0),
+(3, "Tabitha", 1, "F", "dark brown", "Bad", 1),
+(4, "Sergeant Brown", 3, "M", "green", "Good", 1),
+(5, "Melissa", 2, "F", "dark brown", "Good", 0),
+(6, "Grinch", 4, "M", "black", "Bad", 1),
+(7, "Wendy", 2, "F", "blue", "Good", 0),
+(8, null, 2, "M", "black", "Bad", 1)
